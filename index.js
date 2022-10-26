3eebd8ac1f2a47cc649115ac98b2edecd90d29c0
Forhad Khan <fk.nafi11@gmail.com>
2022-10-26 13:28:40 +0600

    course data show with id

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses)
 })
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const select = courses.find(course => course.id == id)
+    if (!select) {
+        return res.send('invalid quarry')
+    }
+    res.send(select)
+})
 
 
 app.listen(port, () => {
